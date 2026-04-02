5695fe608b9ab403e753473f208da4d9b9b9d647
Dogreen <zawiaiyad@gmail.com>
2026-04-02 21:18:40 +0200

    Display logo in Tic-Tac Games Player
    Added logo image to the main window.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from tkinter import*
- root = Tk()
+root = Tk()
 root.geometry("900x700")
 root.title("Tic-Tac Games Player")
 root.config(bg='#0B074F')
+photo = PhotoImage(file="logo.png")
+afflogo = Label(root, image=photo)
+afflogo.pack()
 root.mainloop()
